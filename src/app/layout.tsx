95d7fe333c67d17ce8153d4d7ef867dd4c4de308
liyueyue <15901017921@163.com>
2023-11-03 17:36:44 +0800

    ✨ feat: 修改系统名称

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,14 +11,14 @@ export default function RootLayout({ children }: any) {
       <head>
         {/* ios */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-title" content="PWA test" />
-        <link rel="shortcut icon" sizes="152x152" href="/android-chrome-192x192.png" />
+        <meta name="apple-mobile-web-app-title" content="ChisonNavi" />
+        <link rel="shortcut icon" sizes="152x152" href="/android-chrome-152x152.png" />
         <link rel="shortcut icon" sizes="512x512" href="/android-chrome-512x512.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/android-chrome-192x192.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/android-chrome-152x152.png" />
         <link rel="apple-touch-icon" sizes="512x512" href="/android-chrome-512x512.png" />
-        {/* <link rel="manifest" href="/site.webmanifest" />  Pc */}
+        <link rel="manifest" href="/site.webmanifest" />  {/* Pc */}
         <link rel="manifest" href="/manifest.json" />   {/* 移动 */}
-        <script src="/serviceWorkerRegister.js" async></script>
+        {/* <script src="/serviceWorkerRegister.js" async></script> */}
       </head>
       <body className={'max-h-screen w-full bg-gray-100 dark:bg-zinc-950'}>
         <ChakraProvider theme={theme}>
